ab4305c2dd5fcf56733a4d0926f3ef5b381d9a3a
s1rcody66 <cody200600@gmail.com>
2024-09-25 16:51:58 +0300

    completelab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+const drivers = (["Antonia", "Nuru", "Amari", "Mo"])
+function returnFirstTwoDrivers() {
+    return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers() {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function (fare) {
+        return fare * multiplier
+    }
+}
+
+function fareDoubler(fare) {
+    return (fare * 2);
+}
+
+function fareTripler(fare) {
+    return (fare * 3);
+}
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers) {
+    return ["Antonia", "Nuru"]
+}
+
+function selectDifferentDrivers (drivers,returnLastTwoDrivers) {
+    return ["Amari", "Mo"]
+}
+  
+function selectDifferentDrivers (drivers, cb) {
+    return cb(drivers)
+}
